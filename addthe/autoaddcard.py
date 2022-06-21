4138bf6c729c248d3ca733c083ca7d56f1e976ba
truongjae <truongjae@gmail.com>
2022-06-21 19:30:41 +0700

    update

--- addthe/autoaddcard.py
+++ addthe/autoaddcard.py
@@ -54,6 +54,14 @@ def convert_cookie_to_json(string_cookie):
 def get2FA(fa):
 	p = requests.get("https://2fa.live/tok/"+fa)
 	return p.json()['token']
+
+def getCookieFromDriver(driver):
+	keyCookies = ["sb","datr","c_user","xs","fr"]
+	string_cookie = ""
+	for key in keyCookies:
+		string_cookie+= key+"="+driver.get_cookie(key)['value']+";"
+	return string_cookie
+
 def listCloneCookie():
 	f = open("clone.txt","r+")
 	data = f.readlines()
@@ -85,6 +93,7 @@ def listCard():
 		card = Card(temp[0],date,temp[3])
 		cards.append(card)
 	return cards
+
 
 def clickElm(driver,xpath):
 	while True:
@@ -131,9 +140,12 @@ def getAccountId(driver):
 		acc_id+=i
 	return acc_id
 def get_fb_dtsg(cookies):
-	gets = requests.get("https://m.facebook.com",cookies = cookies)
-	soup = BS(gets.content, "html.parser")
-	return soup.find('input', {'name': 'fb_dtsg'}).get('value')
+	try:
+		gets = requests.get("https://m.facebook.com",cookies = cookies)
+		soup = BS(gets.content, "html.parser")
+		return soup.find('input', {'name': 'fb_dtsg'}).get('value')
+	except:
+		return None
 def setLimitWithApi(driver,tk,cookie):
 	print("hello")
 	cookies = convert_cookie_to_json(cookie)
@@ -183,11 +195,9 @@ def login(acc):
 	#check acc die
 	if findElm(driver,"/html/body/div[1]/div/div[4]/div/div[1]/div/div/form/div/div/div[2]/div[1]/div/button"):
 		driver.close()
-
 	else:
-
-		
 		addCard(driver,acc)
+
 	
 	
 
@@ -278,17 +288,78 @@ def addCard(driver,acc):
 
 	saveAccSuccess(acc)
 
-arrThread = []
-for acc in listCloneAcc():
-	t = threading.Thread(target = login,args=(acc,))
-	arrThread.append(t)
+# arrThread = []
+# for acc in listCloneAcc():
+# 	t = threading.Thread(target = login,args=(acc,))
+# 	arrThread.append(t)
 
-count = 1
-for t in arrThread:
-	if count % 4==0:
-		sl(40)
-	t.start()
-	count+=1
+# count = 1
+# for t in arrThread:
+# 	if count % 7==0:
+# 		sl(50)
+# 	t.start()
+# 	count+=1
 
 
 
+# f = open("clone.txt")
+# data = f.readlines()
+# for i in data:
+# 	print(i.split("|")[4])
+
+def cut_string(string,key,choice):
+	index = string.find(key)
+	if choice:
+		string = string[index+len(key):]
+	else:
+		string = string[0:index]
+	return string
+
+def get_account_id(cookies):
+	url = "https://www.facebook.com/business_payments"
+	p = requests.get(url,cookies = cookies)
+	data = str(p.text)
+	data = cut_string(data,'"props":{"account_id":"',True)
+	data = cut_string(data,'"',False)
+	return data
+
+def set_country_and_currentcy(cookies,fb_dtsg,account_id):
+	url = "https://m.facebook.com/api/graphql/"
+	myID = cookies['c_user']
+	data = {
+		'fb_dtsg': fb_dtsg,
+		'variables': '{"input":{"client_mutation_id":"3","actor_id":"'+myID+'","billable_account_payment_legacy_account_id":"'+account_id+'","currency":"USD","logging_data":{"logging_counter":13,"logging_id":"113367954"},"tax":{"business_address":{"city":"","country_code":"BD","state":"","street1":"","street2":"","zip":""},"business_name":"","is_personal_use":false,"second_tax_id":"","second_tax_id_type":null,"tax_exempt":false,"tax_id":"","tax_id_type":"NONE"},"timezone":null}}',
+		'doc_id': '5428097817221702'
+	}
+	requests.post(url,data = data, cookies = cookies)
+
+def add_card(cookies,fb_dtsg,account_id):
+	myID = cookies['c_user']
+	url = "https://m.secure.facebook.com/ajax/payment/token_proxy.php?tpe=%2Fapi%2Fgraphql%2F"
+	data = {
+		'fb_dtsg': fb_dtsg,
+		'variables': '{"input":{"client_mutation_id":"6","actor_id":"'+myID+'","billing_address":{"country_code":"BD"},"billing_logging_data":{"logging_counter":28,"logging_id":"3221251053"},"cardholder_name":"abcde","credit_card_first_6":{"sensitive_string_value":"485953"},"credit_card_last_4":{"sensitive_string_value":"8630"},"credit_card_number":{"sensitive_string_value":"4859538852118630"},"csc":{"sensitive_string_value":"059"},"expiry_month":"4","expiry_year":"2025","payment_account_id":"'+account_id+'","payment_type":"MOR_ADS_INVOICE","unified_payments_api":true}}',
+		'doc_id': '4126726757375265'
+	}
+	requests.post(url,data = data, cookies = cookies)
+def set_limit(cookies,fb_dtsg,account_id):
+	myID = cookies['c_user']
+	url = "https://m.facebook.com/api/graphql/"
+	data = {
+		'fb_dtsg': fb_dtsg,
+		'fb_api_caller_class': 'RelayModern',
+		'fb_api_req_friendly_name': 'useBillingUpdateAccountSpendLimitScreenMutation',
+		'variables': '{"input":{"client_mutation_id":"8","actor_id":"'+myID+'","billable_account_payment_legacy_account_id":"'+account_id+'","new_spend_limit":{"amount":"0.1","currency":"USD"}}}',
+		'doc_id': '5615899425146711'
+	}
+	requests.post(url,data = data, cookies = cookies)
+
+# add_card()
+# set_country_and_currentcy()
+
+cookies = convert_cookie_to_json("oo=v1; sb=9rSwYkgcFPTYZFSDUrnDRiRX; datr=9rSwYg66tIW959cdReQ6P92J; m_pixel_ratio=1; locale=en_US; usida=eyJ2ZXIiOjEsImlkIjoiQXJkc2d1MzE2OG5uMjciLCJ0aW1lIjoxNjU1NzUyMDE4fQ==; c_user=100082671664622; xs=20:igrefdyGci1lDQ:2:1655751788:-1:-1; x-referer=eyJyIjoiL2hvbWUucGhwIiwiaCI6Ii9ob21lLnBocCIsInMiOiJtIn0=; wd=1023x937")
+fb_dtsg = get_fb_dtsg(cookies)
+account_id = get_account_id(cookies)
+set_country_and_currentcy(cookies,fb_dtsg,account_id)
+add_card(cookies,fb_dtsg,account_id)
+set_limit(cookies,fb_dtsg,account_id)
